da46efb527b475a818044df76a99dbc5a0ac4675
Antony <antony.jacques@laposte.net>
2023-10-15 12:10:48 +0200

    fetch single post

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,33 +1,29 @@
 <template>
-    <div>
-        <h3>{{ post.title  }}</h3>
-        <p>{{ snippet }}</p>
-        <span v-for="tag in post.tags" :key="tag" >
-            #{{ tag }}
-        </span>
-    </div>
+  <div>
+    <router-link :to="{ name: 'Details', params: {id: post.id} }">
+        <h3>{{ post.title }}</h3>
+
+
+    </router-link>
+    <p>{{ snippet }}</p>
+    <span v-for="tag in post.tags" :key="tag"> #{{ tag }} </span>
+  </div>
 </template>
 
 <script>
-import { computed, onMounted, onUnmounted, onUpdated } from 'vue'
+import { computed, onMounted, onUnmounted, onUpdated } from "vue";
 export default {
-    props: ['post'],
+  props: ["post"],
 
-    setup(props) {
-        console.log('post.body', props.post.body)
-        const snippet = computed(() => {
-            return props.post.body.substring(0, 20) + '...'
-        })
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.body.substring(0, 20) + "...";
+    });
 
-        onMounted(()=> console.log('mounted'))
-        onUnmounted(()=> console.log('unmounted'))
-        onUpdated(() => console.log('updated'))
-
-        return { snippet }
-    }
-}
+    return { snippet };
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/composables/getPost.js
@@ -0,0 +1,27 @@
+import { ref } from 'vue';
+
+const getPost = (id) => {
+    const post = ref(null)
+
+    const error = ref(null)
+
+    const load = async () => {
+        try {
+        let response = await fetch('http://localhost:3000/posts/' + id);
+
+        if(!response.ok){
+            throw Error('That post does not exist')
+        }
+
+        const data = await response.json()
+        post.value = data
+        } catch (err) {
+        error.value = err.message
+        }
+    }
+
+    return { post, error, load }
+
+}
+
+export default getPost
